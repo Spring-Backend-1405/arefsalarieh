01a46124c7e96de5274a8b01ee833522a79f8ff5
aref salarieh <arefsalarieh@gmail.com>
2026-06-24 18:39:12 +0330

    add authMiddleware

---
+++ middlewares/authMiddleware.ts
@@ -0,0 +1,51 @@
+import { customError } from "../utils/customError.ts";
+import type { Request, Response, NextFunction } from "express";
+import { checkJwtToken } from "../utils/tokenHelper.ts";
+
+const checkAuthentication = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  let token = req.headers.authorization;
+  token = token?.split(" ")[1];
+  if (!token) customError("first you should login ", 401);
+  else if (token) {
+    const user = checkJwtToken(token);
+    if (!user) customError("your token is not valid", 401);
+    const authUser = user as { id: number; role: string };
+
+    const authReq = req as any;
+
+    authReq.user = {
+      id: authUser.id,
+      role: authUser.role,
+    };
+
+    next();
+  }
+};
+
+const checkAuthorization = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  role: string,
+) => {
+  const authReq = req as any;
+  const user = authReq.user;
+  if (!user) customError("user token is not valid ! please login");
+  if (user.role != role)
+    customError("you don't have access for this action", 403);
+  next();
+};
+
+const checkAuthorizationAdmin = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  checkAuthorization(req, res, next, "admin");
+};
+
+export { checkAuthentication, checkAuthorizationAdmin };
